6ebdee2816f69551dc6230fb7585cb42b8e0d4eb
潘飞 <28442538@qq.com>
2021-02-03 11:46:26 +0800

    'p'

--- src/module-hmmm/components/articles-add.vue
+++ src/module-hmmm/components/articles-add.vue
@@ -6,19 +6,22 @@
       width="50%"
       @close="onIsArticleAddShow"
     >
+      <!-- form表单 -->
       <el-form
         :model="numberValidateForm"
         ref="numberValidateForm"
         label-width="100px"
         class="demo-ruleForm"
       >
+        <!-- 文章标题 -->
         <el-form-item label="文章标题" prop="title" :rules="formRules.title">
           <el-input
             v-model.number="numberValidateForm.title"
             autocomplete="off"
           ></el-input>
         </el-form-item>
-
+        <!-- 文章标题 -->
+        <!-- 富文本编辑器 -->
         <el-form-item
           label="文章内容"
           prop="articleBody"
@@ -32,21 +35,25 @@
             </quill-editor>
           </div>
         </el-form-item>
-
+        <!-- 富文本编辑器 -->
+        <!-- 视频链接 -->
         <el-form-item label="视屏链接" prop="videoURL">
           <el-input
             v-model.number="numberValidateForm.videoURL"
             autocomplete="off"
           ></el-input>
         </el-form-item>
-
+        <!-- 视频链接 -->
+        <!-- 提交取消按钮 -->
         <el-form-item>
           <el-button type="primary" @click="submitForm(numberValidateForm)"
             >提交</el-button
           >
           <el-button @click="resetForm('numberValidateForm')">重置</el-button>
         </el-form-item>
+        <!-- 提交取消按钮 -->
       </el-form>
+      <!-- form表单 -->
     </el-dialog>
   </div>
 </template>
@@ -56,32 +63,41 @@ import { quillEditor } from 'vue-quill-editor' // 调用编辑器
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
-import { add, update } from '@/api/hmmm/articles'
+import { add, update } from '@/api/hmmm/articles' // api
 
 export default {
   props: {
+    // 会话框显示
     isArticleAddShow: {
       type: Boolean
     },
+    // 编辑开关
     edit: {
       type: Boolean
     },
+    // 文章内容
     articls: {
       type: Object,
       required: true
     }
   },
+
   components: {
-    quillEditor
+    quillEditor // 富文本编辑器组件
   },
+
   data() {
     return {
+      // 会话框显示?
       isShowPages: false,
+      // 文章信息
       articlesMsg: {},
+      // 表单验证规则
       formRules: {
         title: [{ required: true, message: '请输入标题' }],
         articleBody: [{ required: true, message: '请输入内容' }]
       },
+      // 表单数据
       numberValidateForm: {
         title: '',
         articleBody: '',
@@ -89,39 +105,53 @@ export default {
       }
     }
   },
-  created() {
-    console.log(321)
-  },
-  mounted() {},
+
   methods: {
+    // 文章怎天显示父组件修改值
     onIsArticleAddShow() {
       this.$emit('update:isArticleAddShow', false)
     },
+
+    // 提交表单数据
     async submitForm(formName) {
       try {
+        // 判断事编辑还事增添状态
         if (this.edit) {
           await update(this.articls)
-          this.$emit('update:edit', false)
+        } else {
+          await add(formName)
         }
-        await add(formName)
 
+        this.$emit('update:edit', false)
         this.isShowPages = false
-        this.resetForm('numberValidateForm')
+        this.resetForm('numberValidateForm') // 重置表单
       } catch (error) {
         console.log(error)
       }
     },
+
+    // 重置表单
     resetForm(formName) {
       this.$refs[formName].resetFields()
     }
   },
-  computed: {},
+
   watch: {
+    // 监听会话框状态
     isArticleAddShow() {
       this.isShowPages = this.isArticleAddShow
-      this.numberValidateForm.title = this.articls.title
-      this.numberValidateForm.articleBody = this.articls.articleBody
-      this.numberValidateForm.videoURL = this.articls.videoURL
+      // 判断是不是编辑状态
+      if (this.edit) {
+        // 编辑状态赋初始值
+        console.log(this.edit)
+        this.numberValidateForm.title = this.articls.title
+        this.numberValidateForm.articleBody = this.articls.articleBody
+        this.numberValidateForm.videoURL = this.articls.videoURL
+      } else {
+        // 增添状态不赋值
+        this.numberValidateForm = {}
+        console.log(this.edit)
+      }
     }
   }
 }
--- src/module-hmmm/pages/articles.vue
+++ src/module-hmmm/pages/articles.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
+    <!-- 卡片主体区域 -->
     <el-card class="box-card">
       <!-- 头部 -->
       <el-form ref="form" :model="form" label-width="80px">
-        <!-- 头部 -->
         <el-row :gutter="20">
           <!-- 输入框 -->
           <el-col :span="6"
@@ -14,7 +14,9 @@
                 @change="onInput"
               ></el-input> </el-form-item
           ></el-col>
+          <!-- 输入框 -->
 
+          <!-- 下拉菜单 -->
           <el-col :span="6"
             ><el-form-item label="状态" class="input">
               <el-select
@@ -31,16 +33,18 @@
                 </el-option>
               </el-select> </el-form-item
           ></el-col>
-          <!-- 输入框 -->
-          <!-- 按钮 -->
+          <!-- 下拉菜单 -->
+
+          <!-- 搜索清除按钮 -->
           <el-col :span="3" style="text-align:right">
             <el-row class="header-button">
               <el-button @click="clearInput">清除</el-button>
               <el-button type="primary" @click="onSearch">搜索</el-button>
             </el-row>
           </el-col>
-          <!-- 按钮 -->
+          <!-- 搜索清除按钮 -->
 
+          <!-- 新增技巧按钮 -->
           <el-col :offset="6" :span="3" style="text-align:right">
             <el-row class="header-button">
               <el-button
@@ -52,10 +56,12 @@
               </el-button>
             </el-row>
           </el-col>
+          <!-- 新增技巧按钮 -->
         </el-row>
-        <!-- 头部 -->
       </el-form>
       <!-- 头部 -->
+
+      <!-- 数据总数 -->
       <el-row>
         <el-col :span="24"
           ><div class="grid-content-total bg-purple-dark">
@@ -64,18 +70,39 @@
           </div></el-col
         >
       </el-row>
+      <!-- 数据总数 -->
+
       <!-- 表格 -->
       <el-table :data="tableData" style="width: 100%" ref="removeId">
+        <!-- 序号行 -->
         <el-table-column type="index" width="50" label="序号">
         </el-table-column>
-        <el-table-column prop="title" label="文章标题"> </el-table-column>
+        <!-- 序号行 -->
+        <!-- 标题行 -->
+        <el-table-column prop="title" label="文章标题">
+          <template slot-scope="scope">
+            <span>{{ scope.row.title }}</span>
+            <span
+              v-show="Boolean(scope.row.videoURL)"
+              class="el-icon-film filmSpan"
+            ></span>
+          </template>
+        </el-table-column>
+        <!-- 标题行 -->
+        <!-- 阅读数行 -->
         <el-table-column prop="visits" label="阅读数"> </el-table-column>
+        <!-- 阅读数行 -->
+        <!-- 录入人行 -->
         <el-table-column prop="username" label="录入人"> </el-table-column>
+        <!-- 录入人行 -->
+        <!-- 状态行 -->
         <el-table-column prop="state" label="状态">
           <template slot-scope="scope"
             >{{ scope.row.state ? '已启用' : '已禁用' }}
           </template>
         </el-table-column>
+        <!-- 状态行 -->
+        <!-- 操作行 -->
         <el-table-column prop="address" label="操作" width="220px">
           <template slot-scope="scope" class="change">
             <el-button type="text" size="small" @click="onPreview(scope.row)"
@@ -100,11 +127,10 @@
             >
           </template>
         </el-table-column>
+        <!-- 操作行 -->
       </el-table>
       <!-- 表格 -->
-      <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
-        <question-page></question-page>
-      </el-dialog>
+
       <!-- 分页 -->
       <div class="block">
         <el-pagination
@@ -120,28 +146,39 @@
       </div>
       <!-- 分页 -->
     </el-card>
-
+    <!-- 卡片主体区域 -->
+    <!-- 文章预览组件 -->
     <articles-preview
       :isArticlePageShow.sync="isArticlesPreviewshow"
       :thisRow="thisRow"
     ></articles-preview>
+    <!-- 文章预览组件 -->
+    <!-- 文章增添组件 -->
     <articles-add
       :isArticleAddShow.sync="isArticlesAddshow"
       :articls="articls"
       :edit="edit"
     ></articles-add>
+    <!-- 文章增添组件 -->
   </div>
 </template>
 
 <script>
-import { list, remove, detail } from '@/api/hmmm/articles'
-import questionPage from './question-page'
-import articlesPreview from '../components/articles-preview'
-import articlesAdd from '../components/articles-add'
+import { list, remove, detail } from '@/api/hmmm/articles' // api接口
+import articlesPreview from '../components/articles-preview' // 文章详情组件
+import articlesAdd from '../components/articles-add' // 文章增添组件
+
 export default {
   data() {
     return {
-      isDisabled: true,
+      // 查询参数
+      query: {
+        page: 1,
+        pagesize: 10,
+        keyword: '',
+        state: null
+      },
+      // 下拉选框
       options: [
         {
           value: 1,
@@ -152,44 +189,45 @@ export default {
           label: '已禁用'
         }
       ],
+      // 下拉菜单数据
       value: '',
+      // 表单数据
       form: {
         name: ''
       },
-      query: {
-        page: 1,
-        pagesize: 10,
-        keyword: '',
-        state: null
-      },
-      total: 0,
+      // 表格数据
       tableData: [],
-      dialogTableVisible: false,
+      // 按钮禁用
+      isDisabled: true,
+      // 列表总数
+      total: 0,
+      // 文章详情显示
       isArticlesPreviewshow: false,
+      // 文章增添显示
       isArticlesAddshow: false,
-      thisRow: {},
-      articls: {},
-      edit: false
+      thisRow: {}, // 当前行信息(组件传值)
+      articls: {}, // 获取的文章信息
+      edit: false // 判断是不是编辑状态
     }
   },
+
   components: {
-    questionPage,
-    articlesPreview,
-    articlesAdd
+    articlesPreview, // 文章详情组件
+    articlesAdd // 文章增添组件
   },
+
   created() {
     this.loadList()
   },
+
   watch: {
     isArticlesAddshow() {
       this.loadList()
     }
   },
+
   methods: {
-    clearInput() {
-      this.form.name = ''
-      this.value = null
-    },
+    // 数据初始化
     async loadList() {
       try {
         const { data } = await list(this.query)
@@ -204,6 +242,13 @@ export default {
       }
     },
 
+    // 清除表单内容
+    clearInput() {
+      this.form.name = ''
+      this.value = null
+    },
+
+    // 删除行
     onRemoveRandoms(row) {
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
         confirmButtonText: '确定',
@@ -232,37 +277,53 @@ export default {
           })
         })
     },
+
+    // 每页数据变化事件
     handleSizeChange(el) {
       this.query.pagesize = el
       this.loadList()
     },
+
+    // 当前页
     handleCurrentChange(el) {
       this.query.page = el
       this.loadList()
     },
+
+    // 输入框数据获取
     onInput() {
       this.query.keyword = this.form.name
     },
+
+    // 搜索事件
     onSearch() {
       this.loadList()
     },
-    onPageShow() {
-      this.dialogTableVisible = true
-    },
-    onIsArticlesAddshow() {
-      this.isArticlesAddshow = true
-    },
+
+    // 按钮禁用事件
     onDisabled(el) {
       console.log(el)
       el.state = !el.state
     },
+
+    // 下拉框选择事件
     selectChange(el) {
       this.query.state = el
     },
+
+    // 文章预览会话框显示事件
     onPreview(row) {
       this.isArticlesPreviewshow = true
       this.thisRow = row
     },
+
+    // 增添文章会话框事件
+    onIsArticlesAddshow() {
+      this.isArticlesAddshow = true
+      this.edit = false
+    },
+
+    // 文章编辑会话框显示事件
     async onEditArticle(row) {
       const { data } = await detail(row)
       this.articls = data
@@ -297,6 +358,14 @@ export default {
         display: inline-block;
       }
     }
+  }
+  // 电影小图标
+  .filmSpan {
+    color: blue;
+    margin-left: 5px;
+    cursor: pointer;
+    font-size: 20px;
+    vertical-align: middle;
   }
 }
 </style>
